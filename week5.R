fbbe7cd5bb10baa1564cc4f064d64075a8b5df9b
vigtak <viggo.takken@gmail.com>
2025-11-27 16:53:38 +0100

    Update week5.R

--- week5.R
+++ week5.R
@@ -97,13 +97,99 @@ abline(coef = c(coef_m2[1] + coef_m2[3], coef_m2[2] + coef_m2[4]),
        col = "purple",
        lwd = 2)
 
-#1.3
 
-monocentric_2016_2 <- read_csv(file = "monocentric_2016") %>%
-  mutate(pricem2 = as.numeric(pricem2),
-         distcbd = as.numeric(distcbd),
-         hhsize = as.numeric(hhsize),
-         popdens = as.numeric(popdens),
-         shhistdistr = as.numeric(shhistdistr))
-model_3 <- lm(pricem2 ~ distcbd, data=data)
-summary(model_3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#1.3.1
+
+# Data opnieuw inlezen
+monocentric_2016 <- read_csv("monocentric_2016.csv") %>%
+  mutate(
+    pricem2 = as.numeric(pricem2),
+    distcbd = as.numeric(distcbd)
+  )
+
+# Dummy voor Nijmegen
+monocentric_2016$nijmegen <- ifelse(monocentric_2016$munname == "Nijmegen", 1, 0)
+monocentric_2016$nijmegen <- as.factor(monocentric_2016$nijmegen)
+
+# Verwijder NA's
+data_full <- monocentric_2016 %>%
+  filter(!is.na(pricem2), !is.na(distcbd))
+
+# Regressie met interactie
+model_interact <- lm(pricem2 ~ distcbd * nijmegen, data = data_full)
+summary(model_interact)
+
+# Plot interactie
+plot_model(model_interact, type = "pred", terms = c("distcbd", "nijmegen"))
+
+# Plotdata voorspellen uit het model
+pred_data <- expand.grid(
+  distcbd = seq(min(data_full$distcbd), max(data_full$distcbd), length.out = 200),
+  nijmegen = levels(data_full$nijmegen)
+)
+
+pred_data$pred <- predict(model_interact, newdata = pred_data)
+
+# Mooie ggplot-interactieplot
+ggplot() +
+  geom_point(data = data_full,
+             aes(x = distcbd, y = pricem2, color = nijmegen),
+             alpha = 0.5) +
+  geom_line(data = pred_data,
+            aes(x = distcbd, y = pred, linetype = nijmegen),
+            linewidth = 1.2,
+            color = "black") +
+  scale_color_manual(values = c("darkgrey", "red"),
+                     labels = c("Niet Nijmegen", "Nijmegen")) +
+  scale_linetype_manual(values = c("solid", "dashed")) + 
+  labs(
+    title = "Interactiemodel: afstand tot CBD × Nijmegen",
+    subtitle = "Verschillende regressielijnen per gemeente",
+    x = "Afstand tot CBD (km)",
+    y = "Prijs per m² (€)",
+    color = "Punten (gemeente)",
+    linetype = "Lijnen (gemeente)"
+  ) +
+  theme_minimal(base_size = 14)
+
+
+
+
+
+
+
+#1.3.3
+
+data_full$distance <- data_full$distcbd
+data_full$nijmegen_dummy <- data_full$nijmegen
+data_full$d_interaction <- data_full$distcbd * data_full$nijmegen
+data_full$population_density <- data_full$popdens
+
+basic_model <- lm(pricem2 ~ distance * nijmegen_dummy, data = data_full)
+
+extended_model <- lm(pricem2 ~ distance * nijmegen_dummy + population_density, 
+                     data = data_full)
+
+stargazer(
+  basic_model,
+  extended_model,
+  type = "text",
+  column.labels = c("Basic", "Extended"),
+  dep.var.labels = "Price per m2"
+)
+
+
